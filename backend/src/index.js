c8d3e3ed02a4684526c2d4a8fc8741f97cebee15
Marcin Wąsik <marcinmw99@gmail.com>
2018-11-13 13:47:54 +0100

    Change server port, add frontend url to authorization

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,15 @@
+require("dotenv").config({ path: "variables.env" });
 import server from "./createServer";
 
-server.start(deets => {
-  console.log(`Server is now running on port http:/localhost:${deets.port}`);
-});
+server.start(
+  {
+    cors: {
+      credentials: true,
+      origin: process.env.FRONTEND_URL
+    },
+    port: process.env.PORT
+  },
+  deets => {
+    console.log(`Server is now running on port http:/localhost:${deets.port}`);
+  }
+);
