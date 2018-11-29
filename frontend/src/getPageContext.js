ca2c662c91d38af3894ea180201061c5893d3ba0
Marcin Wąsik <marcinmw99@gmail.com>
2018-11-29 18:20:55 +0100

    Change color theme

--- frontend/src/getPageContext.js
+++ frontend/src/getPageContext.js
@@ -3,19 +3,19 @@ import {
   createMuiTheme,
   createGenerateClassName
 } from "@material-ui/core/styles";
-import { purple, green } from "@material-ui/core/colors";
+import { red, purple } from "@material-ui/core/colors";
 
 const theme = createMuiTheme({
   palette: {
     primary: {
+      light: red[300],
+      main: red[500],
+      dark: red[700]
+    },
+    secondary: {
       light: purple[300],
       main: purple[500],
       dark: purple[700]
-    },
-    secondary: {
-      light: green[300],
-      main: green[500],
-      dark: green[700]
     }
   },
   typography: {
