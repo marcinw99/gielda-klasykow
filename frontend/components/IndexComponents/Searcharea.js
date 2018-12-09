6d382e509e96e7d5ce1a54eb421756eedb19df9c
Marcin Wąsik <marcinmw99@gmail.com>
2018-12-09 15:22:17 +0100

    Add basic filters to Searcharea

--- frontend/components/IndexComponents/Searcharea.js
+++ frontend/components/IndexComponents/Searcharea.js
@@ -6,23 +6,59 @@ import {
   Select,
   MenuItem,
   TextField,
-  InputAdornment
+  InputAdornment,
+  Paper,
+  Button
 } from "@material-ui/core";
+import { Search, Clear } from "@material-ui/icons";
 import { withStyles } from "@material-ui/core/styles";
 
 const styles = theme => ({
-  root: {
+  form: {
     display: "flex",
-    flexWrap: "wrap"
+    flexWrap: "wrap",
+    justifyContent: "space-between"
   },
   formControl: {
-    minWidth: 120,
+    minWidth: 160,
+    margin: theme.spacing.unit
+  },
+  formControlSmallLeft: {
+    minWidth: 160,
+    margin: theme.spacing.unit,
+    marginRight: theme.spacing.unit * 0.5
+  },
+  formControlSmallRight: {
+    minWidth: 160,
+    margin: theme.spacing.unit,
+    marginLeft: theme.spacing.unit * 0.5
+  },
+  InputLabel: {
+    color: theme.palette.primary.main
+  },
+  button: {
+    minWidth: 140,
+    margin: theme.spacing.unit
+  },
+  fabButton: {
     margin: theme.spacing.unit
   }
 });
 
 class Searcharea extends Component {
-  state = { segment: "", brand: "", model: "", priceFrom: null, priceTo: null };
+  state = {
+    segment: "",
+    brand: "",
+    model: "",
+    priceFrom: "",
+    priceTo: "",
+    mileageFrom: "",
+    mileageTo: "",
+    productionYearFrom: "",
+    productionYearTo: "",
+    fuelType: "",
+    localization: ""
+  };
 
   handleChange = event => {
     this.setState({ [event.target.name]: event.target.value });
@@ -31,9 +67,11 @@ class Searcharea extends Component {
   render() {
     const { classes } = this.props;
     return (
-      <form className={classes.root}>
+      <form className={classes.form}>
         <FormControl className={classes.formControl}>
-          <InputLabel htmlFor="segment">Segment</InputLabel>
+          <InputLabel className={classes.InputLabel} htmlFor="segment">
+            Segment
+          </InputLabel>
           <Select
             value={this.state.segment}
             onChange={this.handleChange}
@@ -54,7 +92,9 @@ class Searcharea extends Component {
           </Select>
         </FormControl>
         <FormControl className={classes.formControl}>
-          <InputLabel htmlFor="brand">Marka pojazdu</InputLabel>
+          <InputLabel className={classes.InputLabel} htmlFor="brand">
+            Marka pojazdu
+          </InputLabel>
           <Select
             value={this.state.brand}
             onChange={this.handleChange}
@@ -73,7 +113,9 @@ class Searcharea extends Component {
           </Select>
         </FormControl>
         <FormControl className={classes.formControl}>
-          <InputLabel htmlFor="model">Model pojazdu</InputLabel>
+          <InputLabel className={classes.InputLabel} htmlFor="model">
+            Model pojazdu
+          </InputLabel>
           <Select
             value={this.state.model}
             onChange={this.handleChange}
@@ -87,30 +129,176 @@ class Searcharea extends Component {
             </MenuItem>
           </Select>
         </FormControl>
+        <div>
+          <FormControl className={classes.formControlSmallLeft}>
+            <TextField
+              name="productionYearFrom"
+              label="Rok produkcji od"
+              value={this.state.productionYearFrom}
+              onChange={this.handleChange}
+              type="number"
+              inputProps={{
+                min: 0,
+                max: 10000000
+              }}
+            />
+          </FormControl>
+          <FormControl className={classes.formControlSmallRight}>
+            <TextField
+              name="productionYearTo"
+              label="Rok produkcji do"
+              value={this.state.productionYearTo}
+              onChange={this.handleChange}
+              type="number"
+              inputProps={{
+                min: 0,
+                max: 10000000
+              }}
+            />
+          </FormControl>
+        </div>
         <FormControl className={classes.formControl}>
-          <TextField
-            name="priceFrom"
-            label="Cena od"
-            value={this.state.priceFrom}
+          <InputLabel className={classes.InputLabel} htmlFor="fuelType">
+            Rodzaj paliwa
+          </InputLabel>
+          <Select
+            value={this.state.fuelType}
             onChange={this.handleChange}
-            type="number"
-            InputProps={{
-              endAdornment: <InputAdornment position="end">zł</InputAdornment>
+            inputProps={{
+              name: "fuelType",
+              id: "fuelType"
             }}
-          />
+          >
+            <MenuItem value="">
+              <em>Wszystkie</em>
+            </MenuItem>
+            <MenuItem value="Benzyna">Benzyna</MenuItem>
+            <MenuItem value="Benzyna + LPG">Benzyna + LPG</MenuItem>
+            <MenuItem value="Benzyna + CNG">Benzyna + CNG</MenuItem>
+            <MenuItem value="Diesel">Diesel</MenuItem>
+            <MenuItem value="Hybryda">Hybryda</MenuItem>
+            <MenuItem value="Elektryczny">Elektryczny</MenuItem>
+          </Select>
         </FormControl>
-        <FormControl className={classes.formControl}>
-          <TextField
-            name="priceTo"
-            label="Cena do"
-            value={this.state.priceTo}
-            onChange={this.handleChange}
-            type="number"
-            InputProps={{
-              endAdornment: <InputAdornment position="end">zł</InputAdornment>
-            }}
-          />
-        </FormControl>
+        <div>
+          <FormControl className={classes.formControlSmallLeft}>
+            <TextField
+              name="priceFrom"
+              label="Cena od"
+              value={this.state.priceFrom}
+              onChange={this.handleChange}
+              type="number"
+              inputProps={{
+                min: 0,
+                max: 10000000
+              }}
+              InputProps={{
+                endAdornment: <InputAdornment position="end">zł</InputAdornment>
+              }}
+            />
+          </FormControl>
+          <FormControl className={classes.formControlSmallRight}>
+            <TextField
+              name="priceTo"
+              label="Cena do"
+              value={this.state.priceTo}
+              onChange={this.handleChange}
+              type="number"
+              inputProps={{
+                min: 0,
+                max: 10000000
+              }}
+              InputProps={{
+                endAdornment: <InputAdornment position="end">zł</InputAdornment>
+              }}
+            />
+          </FormControl>
+        </div>
+        <div>
+          <FormControl className={classes.formControlSmallLeft}>
+            <TextField
+              name="mileageFrom"
+              label="Przebieg od"
+              value={this.state.mileageFrom}
+              onChange={this.handleChange}
+              type="number"
+              inputProps={{
+                min: 0,
+                max: 10000000
+              }}
+              InputProps={{
+                endAdornment: <InputAdornment position="end">km</InputAdornment>
+              }}
+            />
+          </FormControl>
+          <FormControl className={classes.formControlSmallRight}>
+            <TextField
+              name="mileageTo"
+              label="Przebieg do"
+              value={this.state.mileageTo}
+              onChange={this.handleChange}
+              type="number"
+              inputProps={{
+                min: 0,
+                max: 10000000
+              }}
+              InputProps={{
+                endAdornment: <InputAdornment position="end">km</InputAdornment>
+              }}
+            />
+          </FormControl>
+        </div>
+        <Button className={classes.button} variant="outlined" color="primary">
+          Silnik i napęd
+        </Button>
+        <Button className={classes.button} variant="outlined" color="primary">
+          Nadwozie
+        </Button>
+        <Button className={classes.button} variant="outlined" color="primary">
+          Dodatkowe wyposażenie
+        </Button>
+        <Button className={classes.button} variant="outlined" color="primary">
+          Status pojazdu
+        </Button>
+        <Button className={classes.button} variant="outlined" color="primary">
+          Informacje finansowe
+        </Button>
+        <div>
+          <FormControl className={classes.formControl}>
+            <InputLabel className={classes.InputLabel} htmlFor="localization">
+              Miejscowość
+            </InputLabel>
+            <Select
+              value={this.state.localization}
+              onChange={this.handleChange}
+              inputProps={{
+                name: "localization",
+                id: "localization"
+              }}
+            >
+              <MenuItem value="">
+                <em>Wszystkie</em>
+              </MenuItem>
+              <MenuItem value="Dębica">Dębica</MenuItem>
+            </Select>
+          </FormControl>
+          <FormControl className={classes.formControl}>
+            <TextField
+              name="keywords"
+              label="Słowa kluczowe"
+              value={this.state.keywords}
+              onChange={this.handleChange}
+            />
+          </FormControl>
+        </div>
+        <div>
+          <Button color="secondary" variant="fab" className={classes.fabButton}>
+            <Clear />
+          </Button>
+          <Button color="primary" variant="fab" className={classes.fabButton}>
+            <Search />
+          </Button>
+        </div>
       </form>
     );
   }
