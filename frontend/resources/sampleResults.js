a69173f9bdc93021bc0a963f914e9968661720d1
Marcin Wąsik <marcinmw99@gmail.com>
2018-12-09 16:43:43 +0100

    Add example data from backend

---
+++ frontend/resources/sampleResults.js
@@ -0,0 +1,53 @@
+export default [
+  {
+    segment: "C",
+    brand: "Saab",
+    model: "9-3 Turbo X",
+    version: "",
+    price: 49000,
+    mileage: 105000,
+    productionYear: 2009,
+    fuelType: "benzyna",
+    engineAndTransmission: {
+      engineSize: 2789,
+      power: 280,
+      torque: 340
+    },
+    localization: "Dębica",
+    avatar: "/static/samplecar1.jpg"
+  },
+  {
+    segment: "D",
+    brand: "Volkswagen",
+    model: "Passat",
+    version: "B5",
+    price: 1500,
+    mileage: 5000,
+    productionYear: 2003,
+    fuelType: "diesel",
+    engineAndTransmission: {
+      engineSize: 1899,
+      power: 110,
+      torque: 220
+    },
+    localization: "Rzeszów",
+    avatar: "/static/samplecar2.jpg"
+  },
+  {
+    segment: "B",
+    brand: "Audi",
+    model: "A3",
+    version: "8l",
+    price: 11000,
+    mileage: 250000,
+    productionYear: 2002,
+    fuelType: "benzyna + LPG",
+    engineAndTransmission: {
+      engineSize: 1598,
+      power: 202,
+      torque: 210
+    },
+    localization: "Dębica",
+    avatar: "/static/samplecar3.jpg"
+  }
+];
