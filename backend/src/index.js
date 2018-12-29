3ed425c820528db7f474b9e2ea044a2e28c44cca
Marcin Wąsik <marcinmw99@gmail.com>
2018-12-29 14:23:27 +0100

    Get token from user's cookies and add it on his request

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,19 @@
 import cookieParser from "cookie-parser";
+import jwt from "jsonwebtoken";
 
 import server from "./createServer";
 require("dotenv").config({ path: "variables.env" });
 
 server.express.use(cookieParser());
+
+server.express.use((req, res, next) => {
+  const { token } = req.cookies;
+  if (token) {
+    const { userId } = jwt.verify(token, process.env.APP_SECRET);
+    req.userId = userId;
+  }
+  next();
+});
 
 server.start(
   {
