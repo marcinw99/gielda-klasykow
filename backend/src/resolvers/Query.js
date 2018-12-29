88d8a9d6e9b7b5ec3b12d11fad3f017567ca7505
Marcin Wąsik <marcinmw99@gmail.com>
2018-12-29 14:24:22 +0100

    Add thisUser query method to fetch info about logged in user

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -4,7 +4,18 @@ const Query = {
   car: forwardTo("db"),
   post: forwardTo("db"),
   cars: forwardTo("db"),
-  posts: forwardTo("db")
+  posts: forwardTo("db"),
+  thisUser(parent, args, ctx, info) {
+    if (!ctx.request.userId) {
+      return null;
+    }
+    return ctx.db.query.user(
+      {
+        where: { id: ctx.request.userId }
+      },
+      info
+    );
+  }
 };
 
 export default Query;
