cb35eaa8e56cde862f591c0ef62edb745c336c8a
Marcin Wąsik <marcinmw99@gmail.com>
2019-01-14 17:45:45 +0100

    Return mutation for logOut when user is logged in on setnewpassword page

--- frontend/pages/setnewpassword.js
+++ frontend/pages/setnewpassword.js
@@ -1,5 +1,53 @@
 import React, { Component } from "react";
 import Router from "next/router";
+import { Mutation } from "react-apollo";
+import {
+  Button,
+  Paper,
+  FormControl,
+  Input,
+  InputLabel,
+  Typography,
+  withStyles
+} from "@material-ui/core";
+
+import { SIGNOUT_MUTATION } from "../src/Mutations/HeaderMutations";
+import { CURRENT_USER_QUERY } from "../src/Queries/User";
+
+const RootComponent = props => {
+  if (props.thisUser) {
+    return (
+      <Mutation
+        mutation={SIGNOUT_MUTATION}
+        refetchQueries={[
+          {
+            query: CURRENT_USER_QUERY
+          }
+        ]}
+      >
+        {send => {
+          send();
+          return null;
+        }}
+      </Mutation>
+    );
+  }
+  return <SetNewPassword />;
+};
+
+RootComponent.getInitialProps = async function(ctx) {
+  if (shouldRedirect(ctx.query)) {
+    if (ctx.res) {
+      ctx.res.writeHead(302, {
+        Location: "/"
+      });
+      ctx.res.end();
+    } else {
+      Router.push("/");
+    }
+  }
+  return {};
+};
 
 function shouldRedirect(query) {
   if (!query.token || query.token.length !== 10) {
@@ -7,34 +55,97 @@ function shouldRedirect(query) {
   } else return false;
 }
 
-class setnewpassword extends Component {
-  constructor(props) {
-    super(props);
-    // Redirect if someone is logged in
-    if (props.data.thisUser) {
-      Router.push("/");
-    }
-  }
-  static async getInitialProps({ res, query }) {
-    if (shouldRedirect(query)) {
-      if (res) {
-        res.writeHead(302, {
-          Location: "/"
-        });
-        res.end();
-      } else {
-        Router.push("/");
-      }
-    }
-    return {};
-  }
+// Code above performs data validation and logs out when is user is logged in
 
-  render() {
-    if (this.props.data.thisUser) {
-      Router.push("/");
-    }
-    return <div>New password</div>;
-  }
-}
+// Code below handles password setting
 
-export default setnewpassword;
+const styles = theme => ({
+  container: {
+    width: "auto",
+    display: "block",
+    margin: `${theme.spacing.unit * 3} auto 0 auto`,
+    [theme.breakpoints.up(400 + theme.spacing.unit * 3 * 2)]: {
+      width: 400,
+      marginLeft: "auto",
+      marginRight: "auto"
+    }
+  },
+  form: {
+    width: "100%"
+  },
+  paper: {
+    marginTop: theme.spacing.unit * 8,
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 3}px ${theme
+      .spacing.unit * 3}px`
+  },
+  submit: {
+    marginTop: theme.spacing.unit * 3
+  }
+});
+
+const SetNewPassword = withStyles(styles)(
+  class SetNewPassword extends Component {
+    state = {
+      password: "",
+      passwordRepeat: ""
+    };
+
+    handleChange = event => {
+      this.setState({
+        [event.target.name]: event.target.value
+      });
+    };
+
+    render() {
+      const { classes } = this.props;
+      return (
+        <main className={classes.container}>
+          <Paper className={classes.paper}>
+            <Typography variant="h4" color="primary">
+              Resetowanie hasła
+            </Typography>
+            <form className={classes.form}>
+              <FormControl margin="normal" required fullWidth>
+                <InputLabel htmlFor="password">Wpisz nowe hasło</InputLabel>
+                <Input
+                  type="password"
+                  id="password"
+                  name="password"
+                  value={this.state.password}
+                  onChange={this.handleChange}
+                  autoFocus
+                />
+              </FormControl>
+              <FormControl margin="normal" required fullWidth>
+                <InputLabel htmlFor="passwordRepeat">
+                  Potwierdź hasło
+                </InputLabel>
+                <Input
+                  type="password"
+                  id="passwordRepeat"
+                  name="passwordRepeat"
+                  value={this.state.passwordRepeat}
+                  onChange={this.handleChange}
+                />
+              </FormControl>
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                color="primary"
+                className={classes.submit}
+              >
+                Zmień hasło
+              </Button>
+            </form>
+          </Paper>
+        </main>
+      );
+    }
+  }
+);
+
+export default RootComponent;
