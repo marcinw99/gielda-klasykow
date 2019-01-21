96e7ddf097878bb1140baef73808ad6ec288222c
Marcin Wąsik <marcinmw99@gmail.com>
2019-01-21 16:18:33 +0100

    Add query to fetch models of given brand

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,4 +1,5 @@
 const { forwardTo } = require("prisma-binding");
+import { CarQuery } from "car-query";
 
 import messageCodes from "../messageCodes";
 
@@ -7,23 +8,31 @@ const Query = {
   post: forwardTo("db"),
   cars: forwardTo("db"),
   posts: forwardTo("db"),
-  thisUser(parent, args, ctx, info) {
-    if (!ctx.request.userId) {
+  thisUser(parent, args, context, info) {
+    if (!context.request.userId) {
       return null;
     }
-    return ctx.db.query.user(
+    return context.db.query.user(
       {
-        where: { id: ctx.request.userId }
+        where: { id: context.request.userId }
       },
       info
     );
   },
-  async resetTokenValid(parent, { resetToken }, ctx, info) {
-    const [user] = await ctx.db.query.users({ where: { resetToken } });
+  async resetTokenValid(parent, { resetToken }, context, info) {
+    const [user] = await context.db.query.users({ where: { resetToken } });
     if (!user) {
       return { code: messageCodes.resetLinkExpiredOrInvalid };
     }
     return { code: messageCodes.resetTokenCorrect };
+  },
+  async modelsOfBrand(parent, args, context, info) {
+    var results = [];
+    const carQuery = new CarQuery();
+    await carQuery.getModels({ make: args.brand }).then(models => {
+      results = models.map(item => item.name);
+    });
+    return results;
   }
 };
 
