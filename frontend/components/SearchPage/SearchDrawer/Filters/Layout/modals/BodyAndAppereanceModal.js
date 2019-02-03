e3cc019be0c3b57378219c68931e5afcb45fd413
Marcin Wąsik <marcinmw99@gmail.com>
2019-02-03 21:22:06 +0100

    refactoring

--- frontend/components/SearchPage/SearchDrawer/Filters/Layout/modals/BodyAndAppereanceModal.js
+++ frontend/components/SearchPage/SearchDrawer/Filters/Layout/modals/BodyAndAppereanceModal.js
@@ -31,92 +31,89 @@ const styles = theme => ({
   }
 });
 
-const BodyAndAppereanceModal = props => {
-  console.log(props.selectsOptions);
-  return (
-    <Modal
-      aria-labelledby="body-and-appereance-filters"
-      open={props.openedModal === "BodyAndAppereanceModal"}
-      onClose={props.closeModal}
-    >
-      <ModalPaper>
-        <Header title="Nadwozie" handleClose={props.closeModal} />
-        <ModalContent>
-          <StyledFilterTitle>Kolor lakieru</StyledFilterTitle>
-          <FormControl>
-            <Grid container justify="flex-start">
-              {props.selectsOptions.Color.map(item => (
-                <FormControlLabel
-                  className={props.classes.formControlLabel}
-                  key={item}
-                  control={
-                    <Checkbox
-                      checked={props.values.color_in.indexOf(item) !== -1}
-                      onChange={props.handleMultiCheckboxChange("color_in")}
-                      value={item}
-                    />
-                  }
-                  label={enumsDisplayedText("color", item)}
-                />
-              ))}
-            </Grid>
-          </FormControl>
-          <StyledFilterTitle>
-            Kierownica po prawej stronie (anglik)
-          </StyledFilterTitle>
-          <FormControl className={props.classes.formControl}>
-            <Select
-              value={props.values.steeringWheelOnTheRight}
-              onChange={e => props.handleChangeWithoutFiltering(e.target)}
-              inputProps={{
-                name: "steeringWheelOnTheRight"
-              }}
-            >
-              <MenuItem value="deleteFromFilters">
-                <em>Nie wybrano</em>
-              </MenuItem>
-              <MenuItem value={true}>Tak</MenuItem>
-              <MenuItem value={false}>Nie</MenuItem>
-            </Select>
-          </FormControl>
-          <StyledFilterTitle>Wygląd</StyledFilterTitle>
-          <FormControl>
-            <Grid container justify="flex-start">
-              {props.selectsOptions.AdditionalAccessory_Appereance.map(item => (
-                <FormControlLabel
-                  className={props.classes.formControlLabel}
-                  key={item}
-                  control={
-                    <Checkbox
-                      checked={
-                        props.values.AdditionalAccessory_Appereance_in.indexOf(
-                          item
-                        ) !== -1
-                      }
-                      onChange={props.handleMultiCheckboxChange(
-                        "AdditionalAccessory_Appereance_in"
-                      )}
-                      value={item}
-                    />
-                  }
-                  label={enumsDisplayedText(
-                    "AdditionalAccessory_Appereance",
-                    item
-                  )}
-                />
-              ))}
-            </Grid>
-          </FormControl>
-        </ModalContent>
-        <Footer
-          handleSubmit={props.submitModal}
-          handleReset={() => {
-            props.resetSpecificFiltersWithoutFiltering(blankFiltersStateBody);
-          }}
-        />
-      </ModalPaper>
-    </Modal>
-  );
-};
+const BodyAndAppereanceModal = props => (
+  <Modal
+    aria-labelledby="body-and-appereance-filters"
+    open={props.openedModal === "BodyAndAppereanceModal"}
+    onClose={props.closeModal}
+  >
+    <ModalPaper>
+      <Header title="Nadwozie" handleClose={props.closeModal} />
+      <ModalContent>
+        <StyledFilterTitle>Kolor lakieru</StyledFilterTitle>
+        <FormControl>
+          <Grid container justify="flex-start">
+            {props.selectsOptions.Color.map(item => (
+              <FormControlLabel
+                className={props.classes.formControlLabel}
+                key={item}
+                control={
+                  <Checkbox
+                    checked={props.values.color_in.indexOf(item) !== -1}
+                    onChange={props.handleMultiCheckboxChange("color_in")}
+                    value={item}
+                  />
+                }
+                label={enumsDisplayedText("color", item)}
+              />
+            ))}
+          </Grid>
+        </FormControl>
+        <StyledFilterTitle>
+          Kierownica po prawej stronie (anglik)
+        </StyledFilterTitle>
+        <FormControl className={props.classes.formControl}>
+          <Select
+            value={props.values.steeringWheelOnTheRight}
+            onChange={e => props.handleChangeWithoutFiltering(e.target)}
+            inputProps={{
+              name: "steeringWheelOnTheRight"
+            }}
+          >
+            <MenuItem value="deleteFromFilters">
+              <em>Nie wybrano</em>
+            </MenuItem>
+            <MenuItem value={true}>Tak</MenuItem>
+            <MenuItem value={false}>Nie</MenuItem>
+          </Select>
+        </FormControl>
+        <StyledFilterTitle>Wygląd</StyledFilterTitle>
+        <FormControl>
+          <Grid container justify="flex-start">
+            {props.selectsOptions.AdditionalAccessory_Appereance.map(item => (
+              <FormControlLabel
+                className={props.classes.formControlLabel}
+                key={item}
+                control={
+                  <Checkbox
+                    checked={
+                      props.values.AdditionalAccessory_Appereance_in.indexOf(
+                        item
+                      ) !== -1
+                    }
+                    onChange={props.handleMultiCheckboxChange(
+                      "AdditionalAccessory_Appereance_in"
+                    )}
+                    value={item}
+                  />
+                }
+                label={enumsDisplayedText(
+                  "AdditionalAccessory_Appereance",
+                  item
+                )}
+              />
+            ))}
+          </Grid>
+        </FormControl>
+      </ModalContent>
+      <Footer
+        handleSubmit={props.submitModal}
+        handleReset={() => {
+          props.resetSpecificFiltersWithoutFiltering(blankFiltersStateBody);
+        }}
+      />
+    </ModalPaper>
+  </Modal>
+);
 
 export default withStyles(styles)(BodyAndAppereanceModal);
