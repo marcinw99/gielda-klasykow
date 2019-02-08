7c2fc9e0c6acfa624385a7b3a2d955eaad143a64
Marcin Wąsik <marcinmw99@gmail.com>
2019-02-08 16:31:44 +0100

    Complete AdditionalAccessories form step

--- frontend/components/AddPage/Form/AdditionalAccessories.js
+++ frontend/components/AddPage/Form/AdditionalAccessories.js
@@ -1,7 +1,45 @@
 import React from "react";
+import { Grid } from "@material-ui/core";
 
-const AdditionalAccessories = () => {
-  return <div>AdditionalAccessories AdditionalAccessories</div>;
-};
+import { StyledValueTitle, StyledFormControl } from "../styledComponents";
+import CheckboxGenerator from "../../universal/CheckboxGenerator";
+
+const AdditionalAccessories = props => (
+  <div>
+    <StyledValueTitle>Bezpieczeństwo</StyledValueTitle>
+    <StyledFormControl>
+      <Grid container justify="flex-start">
+        <CheckboxGenerator
+          name="additionalAccessories_Safety"
+          options={props.options.additionalAccessories_Safety}
+          values={props.values.additionalAccessories_Safety}
+          handleChange={props.handleMultiCheckboxChange}
+        />
+      </Grid>
+    </StyledFormControl>
+    <StyledValueTitle>Komfort kierowcy</StyledValueTitle>
+    <StyledFormControl>
+      <Grid container justify="flex-start">
+        <CheckboxGenerator
+          name="additionalAccessories_Comfort_Driver"
+          options={props.options.additionalAccessories_Comfort_Driver}
+          values={props.values.additionalAccessories_Comfort_Driver}
+          handleChange={props.handleMultiCheckboxChange}
+        />
+      </Grid>
+    </StyledFormControl>
+    <StyledValueTitle>Komfort pasażerów</StyledValueTitle>
+    <StyledFormControl>
+      <Grid container justify="flex-start">
+        <CheckboxGenerator
+          name="additionalAccessories_Comfort_Passenger"
+          options={props.options.additionalAccessories_Comfort_Passenger}
+          values={props.values.additionalAccessories_Comfort_Passenger}
+          handleChange={props.handleMultiCheckboxChange}
+        />
+      </Grid>
+    </StyledFormControl>
+  </div>
+);
 
 export default AdditionalAccessories;
