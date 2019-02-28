cdec85bf620a77d088035591e93d59e1eaef1136
Marcin Wąsik <marcinmw99@gmail.com>
2019-02-28 20:36:06 +0100

    Include dev and prod origin in cors, update endpoint

--- frontend/config.js
+++ frontend/config.js
@@ -1,4 +1,4 @@
 // public client side config
 export const endpoint = `http://localhost:4444`;
-export const prodEndpoint = `https://gieldaklasykow-backend-mlcagdpknl.now.sh/`;
+export const prodEndpoint = `https://gieldaklasykowbackend.now.sh/`;
 export const perPage = 4;
