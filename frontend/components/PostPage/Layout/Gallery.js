8122b8f46d192538e388b7368b6628ca7c1b02a3
Marcin Wąsik <marcinmw99@gmail.com>
2019-03-23 14:51:11 +0100

    Display full sized image on click in postPage's gallery

--- frontend/components/PostPage/Layout/Gallery.js
+++ frontend/components/PostPage/Layout/Gallery.js
@@ -1,5 +1,13 @@
-import React from "react";
-import { GridList, GridListTile, withStyles } from "@material-ui/core";
+import React, { Component, Fragment } from "react";
+import {
+  GridList,
+  GridListTile,
+  Modal,
+  withStyles,
+  Paper,
+  Fab
+} from "@material-ui/core";
+import { Close } from "@material-ui/icons";
 
 const styles = theme => ({
   avatar: {
@@ -11,29 +19,95 @@ const styles = theme => ({
     width: "100%",
     maxHeight: "80vh",
     overflowY: "auto"
+  },
+  fullSizeImage: {
+    height: "100%",
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "center",
+    backgroundSize: "contain"
+  },
+  paper: {
+    position: "relative",
+    outline: "none",
+    marginTop: "2.5vh",
+    marginLeft: "auto",
+    marginRight: "auto",
+    width: "90vw",
+    height: "95vh"
+  },
+  closeBtn: {
+    position: "absolute",
+    top: "2%",
+    right: "2%"
   }
 });
 
-const Gallery = props => (
-  <div>
-    <img
-      className={props.classes.avatar}
-      src={props.avatar || "/static/noImageAvailable.jpg"}
-      alt="Zdjęcie ogłoszenia"
-    />
-    <GridList
-      cellHeight={200}
-      spacing={1}
-      cols={4}
-      className={props.classes.gridList}
-    >
-      {props.photos.map(photo => (
-        <GridListTile key={photo} cols={1}>
-          <img src={photo} alt="Samochód" />
-        </GridListTile>
-      ))}
-    </GridList>
-  </div>
-);
+class Gallery extends Component {
+  state = {
+    modalOpen: false,
+    modalImage: "/static/noImageAvailable.jpg"
+  };
+
+  handleModalClose = () => {
+    this.setState({ modalOpen: false });
+  };
+
+  handleModalOpen = src => {
+    this.setState({
+      modalOpen: true,
+      modalImage: src
+    });
+  };
+
+  render() {
+    const { classes } = this.props;
+    return (
+      <Fragment>
+        <div>
+          <img
+            className={classes.avatar}
+            src={this.props.avatar || "/static/noImageAvailable.jpg"}
+            alt="Zdjęcie ogłoszenia"
+          />
+          <GridList
+            cellHeight={200}
+            spacing={1}
+            cols={4}
+            className={classes.gridList}
+          >
+            {this.props.photos.map(photo => (
+              <GridListTile
+                key={photo}
+                cols={1}
+                onClick={() => this.handleModalOpen(photo)}
+              >
+                <img src={photo} alt="Samochód" />
+              </GridListTile>
+            ))}
+          </GridList>
+        </div>
+        <Modal
+          aria-labelledby="gallery-modal"
+          open={this.state.modalOpen}
+          onClose={this.handleModalClose}
+        >
+          <Paper className={classes.paper}>
+            <div
+              className={classes.fullSizeImage}
+              style={{ backgroundImage: `url("${this.state.modalImage}")` }}
+            />
+            <Fab
+              color="secondary"
+              onClick={this.handleModalClose}
+              className={classes.closeBtn}
+            >
+              <Close />
+            </Fab>
+          </Paper>
+        </Modal>
+      </Fragment>
+    );
+  }
+}
 
 export default withStyles(styles)(Gallery);
--- frontend/components/PostPage/Layout/index.js
+++ frontend/components/PostPage/Layout/index.js
@@ -13,17 +13,15 @@ const styles = theme => ({
   }
 });
 
-const Layout = props => {
-  return (
-    <Grid container className={props.classes.root}>
-      <Grid item xs={6}>
-        <Gallery avatar={props.post.avatar} photos={props.post.photos || []} />
-      </Grid>
-      <Grid item xs={6} className={props.classes.infoContainer}>
-        <Info post={props.post} />
-      </Grid>
+const Layout = props => (
+  <Grid container className={props.classes.root}>
+    <Grid item xs={6}>
+      <Gallery avatar={props.post.avatar} photos={props.post.photos || []} />
     </Grid>
-  );
-};
+    <Grid item xs={6} className={props.classes.infoContainer}>
+      <Info post={props.post} />
+    </Grid>
+  </Grid>
+);
 
 export default withStyles(styles)(Layout);
